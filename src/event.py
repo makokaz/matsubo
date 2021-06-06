c6d289c012c98ef57ed832bffb857ca06cb9b226
Marco O <marcoomainska@icloud.com>
2021-06-06 22:16:28 +0900

    Event scrapper working: Tokyo Cheapo, Japan Cheapo

---
+++ src/event.py
@@ -0,0 +1,97 @@
+"""Event class
+
+Simple event class to define what attributes an event has, and other helpful functions like euqlity-checking.
+"""
+
+class Event(object):
+    def __init__(self,
+            id='', # Unique ID for every event
+            name='', # Event name
+            description='', # Event description
+            url='', # URL where event was found
+            img='', # Image URL
+            date='', # Date of event
+            time='', # Time of event
+            location='', # Event-Location
+            cost='', # Entry-fee to event
+            status='', # Cancelled, Online, ...
+            category=''): # Prefecture, University, ... used for visibility to channels
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.img = img
+        self.date = date
+        self.time = time
+        self.location = location
+        self.cost = cost
+        self.status = status
+        self.category = category
+
+    def __eq__(self, other):
+        if isinstance(other, Event):
+            return self.id == other.id
+        return False
+
+    def __str__(self):
+        text = """***{name}*** [{id}]
+        date: {date}
+        time: {time}
+        location: {location}
+        cost: {cost}
+        url: {url}
+        image-url: {img}
+        status: {status}
+        category: {category}
+        description: {description}"""
+        return text.format(id=self.id,
+            name=self.name,
+            description=self.description,
+            url=self.url,
+            img=self.img,
+            date=self.date,
+            time=self.time,
+            location=self.location,
+            cost=self.cost,
+            status=self.status,
+            category=self.category)
+
+def mergeDuplicateEvents(events, check_duplicate_func=None, merge_func=None, verbose=False):
+    """
+    Merges duplicate events in given list.
+    Duplicate events happen when e.g. the same event is hold next week again.
+    
+    Optional arguments:
+        * check_duplicate_func: Pointer to function that checks if two events are identical. [Default: Check by event-ID]
+        * merge_func: Pointer to function that merges the events. [Default: Only merge event-dates]
+        * verbose: Flag, defines if merged events shall be printed
+    """
+    def sameID(eventA, eventB):
+        return eventA.id == eventB.id
+    def mergeDate(eventA, eventB):
+        eventA.date += ' & ' + eventB.date
+        return eventA
+
+    if check_duplicate_func is None:
+        check_duplicate_func = sameID
+    if merge_func is None:
+        merge_func = mergeDate
+
+    # TODO: This function might have a nasty bug. Check if it produces the correct output
+    i = 0
+    while i < len(events):
+        eventA = events[i]
+        j = i + 1
+        while j < len(events):
+            eventB = events[j]
+            if check_duplicate_func(eventA, eventB):
+                eventA = merge_func(eventA, eventB)
+                if verbose:
+                    print("Merged events:\n\teventA: {}\n\teventB: {}".format(eventA.url,eventB.url))
+                del events[j]
+                i -= 1 # Possibly a bug, no?
+                j -= 1
+                # Why is event not set back into the array?
+            j += 1
+        i += 1
+    return events
---
+++ src/event_scrapper.py
@@ -0,0 +1,121 @@
+"""Event scrapper
+
+Scraps events from pre-defined websites.
+"""
+
+import sys
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen
+from event import Event, mergeDuplicateEvents
+
+
+def grabPage(url: str):
+    """Return html-code of a given url as soup"""
+    uClient = urlopen(url)
+    page_html = uClient.read()
+    uClient.close()
+    return soup(page_html, "html.parser")
+
+def getEventsTY():
+    """Return events from Tokyo Cheapo"""
+    # Fetch soup from TokyoCheapo
+    url = 'https://tokyocheapo.com/events/'
+    page = grabPage(url)
+    event_soup = page.findAll("article",{"class":"article card card--event"})
+    # Identify events in soup
+    events = []
+    for event_ in event_soup:
+        event = Event(
+            id=event_.findAll(attrs={"data-post-id" : True})[0]['data-post-id'].strip(),
+            name=event_.findAll("h3", class_="card__title")[0].text.strip(),
+            description=event_.findAll("p", class_="card__excerpt")[0].text.strip(),
+            url=event_.findAll("h3", class_="card__title")[0].a['href'],
+            img=event_.findAll("a",  class_="card__image")[0].img,
+            date=event_.findAll("div", class_="card--event__date-box")[0].div.text.strip().replace("\n"," "),
+            time=', '.join([t.parent.span.text.strip() for t in event_.findAll("div", title="Start/end time")]),
+            location=', '.join([loc.text for loc in event_.findAll("a", class_="location")]),
+            cost=', '.join([cost.parent.text.strip() for cost in event_.findAll("div", title="Entry")]),
+            status=', '.join([stat.text.strip().lower() for stat in event_.findAll("div", class_="event-status")]),
+            category='Kanto')
+        if event.img is not None: # Hotfix
+            event.img = event.img['data-src']
+        events.append(event)
+    # merge duplicate events: Merge date, check by ID
+    #print(len(events))
+    events = mergeDuplicateEvents(events)
+    #print(len(events))
+    return events
+
+def getEventsJC():
+    """Return events from Japan Cheapo"""
+    regions = {
+        'Chubu': ['Niigata','Ishikawa','Fukui','Yamanashi','Nagano','Gifu','Shizuoka','Aichi'],
+        'Chugoku': ['Shimane','Okayama','Hiroshima','Yamaguchi'],
+        'Hokkaido': ['Hokkaido'],
+        'Kansai': ['Mie','Shiga','Kyoto','Osaka','Hyogo','Nara','Wakayama'], # Himeji, Kobe are also options, but are also included in Hyogo
+        'Kanto': ['Tochigi'], # Tokyo, Ibaraki, Gunma... are left out because they are published on Tokyo Cheapo
+        'Kyushu': ['Fukuoka','Saga','Nagasaki','Kumamoto','Oita','Miyazaki'],
+        'Okinawa': ['Okinawa'],
+        'Shikoku': ['Tokushima','Kagawa'],
+        'Tohoku': ['Aomori','Iwate','Miyagi','Akita','Yamagata','Fukushima'],
+        }
+    # setup toolbar
+    WIDTH_PROGRESSBAR = len(regions)
+    sys.stdout.write("Progress: [%s]" % (" " * len(regions)))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (len(regions)+1)) # return to start of line, after '['
+    # Crawl events
+    events = []
+    for i, region in enumerate(regions):
+        for prefecture in regions[region]:
+            # Fetch soup from JapanCheapo
+            url = 'https://japancheapo.com/events/location/' + prefecture.lower()
+            page = grabPage(url)
+            event_soup = page.findAll("article",{"class":"article card card--event"})
+            # Identify events in soup
+            prefecture_events = []
+            for event_ in event_soup:
+                event = Event(
+                    id=event_.findAll(attrs={"data-post-id" : True})[0]['data-post-id'].strip(),
+                    name=event_.findAll("h3", class_="card__title")[0].text.strip(),
+                    description=event_.findAll("p", class_="card__excerpt")[0].text.strip(),
+                    url=event_.findAll("h3", class_="card__title")[0].a['href'],
+                    img=event_.findAll("a",  class_="card__image")[0].img,
+                    date=event_.findAll("div", class_="card--event__date-box")[0].div.text.strip().replace("\n"," "),
+                    time=', '.join([t.parent.span.text.strip() for t in event_.findAll("div", title="Start/end time")]),
+                    location=', '.join([loc.text for loc in event_.findAll("a", class_="location")]),
+                    cost=', '.join([cost.parent.text.strip() for cost in event_.findAll("div", title="Entry")]),
+                    status=', '.join([stat.text.strip().lower() for stat in event_.findAll("div", class_="event-status")]),
+                    category=region)
+                if event.img is not None: # Hotfix
+                    event.img = event.img['data-src']
+                prefecture_events.append(event)
+            # merge duplicate events: Merge date, check by ID
+            prefecture_events = mergeDuplicateEvents(prefecture_events)
+            events.extend(prefecture_events)
+        # Update progressbar
+        sys.stdout.write("-")
+        sys.stdout.flush()
+    sys.stdout.write("]\n") # this ends the progress bar
+    return events
+
+
+# START OF PROGRAM
+if __name__ == "__main__":
+    # Crawl events
+    eventsTY = getEventsTY()
+    eventsJC = getEventsJC()
+    # Print events
+    events = eventsTY + eventsJC
+    for event in events:
+       print(event)
+    print('Found {} events in total'.format(len(events)))
+    events = mergeDuplicateEvents(events)
+    print('Found {} events in total'.format(len(events)))
+
+
+# TODO:
+# 1. Save events into Postgres
+# 2. Write Discord bot
+# 3. Extend crawled websites: Global Komaba, Todai ISSR, EMail, Facebook, ...
